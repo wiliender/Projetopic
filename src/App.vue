58af6a10f2425f4bdf3f7a6d97e55e50954227f9
Wiliender <wiliendersilva@gmail.com>
2021-12-12 20:40:37 -0300

    Criação do projeto, implementação de componentes, painel, style e integração com API

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+
+<template>
+<div class="corpo">
+  <h1 class="centralizado">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+          <div class="painel">
+            <h2 class="painel-title">{{ foto.titulo }}</h2>
+            <div class="painel-body">
+              <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo"> 
+            </div>
+          </div>
+      </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: "Vue.JS",
+      fotos: []
+    }
+  },
+
+  created() {
+    this.$http.get("http://localhost:3000/v1/fotos")//executa o metodo get para esse endereço retornando uma promise
+        .then(res => res.json())// para retornar uma promis tem que ser atraves da função then
+        .then(fotos => this.fotos = fotos, err => console.log(err))//se der certo executa o metodo fotos e se der errado executa o console.log;
+        //promise.then(res => {
+        //  res.json().then(fotos => this.fotos = fotos);
+        //});
+  }
+}
+</script>
+
+<style>
+.corpo {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+
+.centralizado {
+  text-align: center;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos-item {
+  display: inline-block;
+}
+
+.imagem-responsiva {
+  width: 100%;
+}
+
+.painel {
+padding: 0 auto;
+border: solid 2px grey;
+display: inline-block;
+margin: 5px;
+box-shadow: 5px 5px 10px grey;
+width: 200px;
+height: 100%;
+vertical-align: top;
+text-align: center;
+}
+
+.painel .painel-titulo {
+text-align: center;
+border: solid 2px;
+background: lightblue;
+margin: 0 0 15px 0;
+padding: 10px;
+text-transform: uppercase;
+}
+</style>
