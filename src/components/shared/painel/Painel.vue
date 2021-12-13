695416df91755f6c0946ffd3eecc4496aa0304e9
Wiliender <wiliendersilva@gmail.com>
2021-12-12 21:12:46 -0300

    creating the painel component, importing and communicating with the component

---
+++ src/components/shared/painel/Painel.vue
@@ -0,0 +1,40 @@
+
+<template>
+    <div class="painel">
+        <h2 class="painel-title">{{ titulo }}</h2>
+        <div class="painel-body">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'titulo'
+    ],
+}
+</script>
+
+<style>
+.painel {
+padding: 0 auto;
+border: solid 2px grey;
+display: inline-block;
+margin: 5px;
+box-shadow: 5px 5px 10px grey;
+width: 200px;
+height: 100%;
+vertical-align: top;
+text-align: center;
+}
+
+.painel .painel-title {
+text-align: center;
+border: solid 2px;
+background: lightblue;
+margin: 0 0 15px 0;
+padding: 10px;
+text-transform: uppercase;
+}
+</style>
