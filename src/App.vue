d93af0a138860dd8184e6af75349a605ef5c578c
Wiliender <wiliendersilva@gmail.com>
2021-12-13 00:23:24 -0300

    Turning the menu into a component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 
 <template>
   <div class="corpo">
-    <nav>
-      <ul>
-        <li v-for="route in routes"><router-link :to="route.path ? route.path : '/'">{{ route.titulo }}</router-link></li>
-      </ul>
-    </nav>
 
-    <router-view></router-view>
+    <meu-menu :rotas="routes"/>
+
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
 
-import { routes } from './routes'
+import { routes }  from './routes';
+import Menu from './components/shared/menu/Menu.vue';
 
 export default {
+
+  components: {
+    'meu-menu' : Menu
+  },
 
     data() {
       return {
@@ -31,6 +35,14 @@ export default {
   font-family: Helvetica, sans-serif;
   width: 96%;
   margin: 0 auto;
+}
+
+.pagina-enter, .pagina-leave-active {
+    opacity: 0;
+}
+
+.pagina-enter-active, .pagina-leave-active {
+    transition: opacity .4s;
 }
 
 </style>
